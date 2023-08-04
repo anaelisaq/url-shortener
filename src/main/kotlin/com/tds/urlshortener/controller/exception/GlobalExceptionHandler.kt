f33b47b7c81d55b07c61ab18c3d48b63d7fdc205
anaelisaq <anaelisa.abs@gmail.com>
2023-08-04 11:09:43 -0300

    build: add global exception handler package

---
+++ src/main/kotlin/com/tds/urlshortener/controller/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,30 @@
+package com.tds.urlshortener.controller.exception
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+import java.rmi.UnexpectedException
+
+@RestController
+class GlobalExceptionHandler {
+    private val logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleBusinessException(ex: IllegalArgumentException): ResponseEntity<String> {
+        return ResponseEntity(ex.message, HttpStatus.UNPROCESSABLE_ENTITY)
+    }
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNoContentException(): ResponseEntity<String> {
+        return ResponseEntity("Resource ID not found.", HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(Throwable::class)
+    fun handleUnexpectedException(unexpectedException: Throwable): ResponseEntity<String> {
+        val message = "Unexpected server error."
+        logger.error(message, unexpectedException)
+        return ResponseEntity(message, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+}
